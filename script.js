bbeda344ae03a1407afc28fc93920e4d6770af9f
kasakkunduchahal-spec <kasakkunduchahal@gmail.com>
2026-09-25 17:22:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,9 @@ const number23 = document.querySelector(".number23");
 const openButton = document.querySelector("#openButton");
 
 number23.addEventListener("click", () => {
-
-  // Activate the animation state
   card.classList.add("active");
-
-  // Wait until the color transition and element animations
-  // have had time to play
-  setTimeout(() => {
-    openButton.classList.add("show");
-  }, 1200);
-
 });
 
-
 openButton.addEventListener("click", () => {
-
-  alert("Stage 1 works! 💗\n\nNext we'll connect this to your envelope page.");
-
+  alert("Stage 1 works! 💗");
 });
